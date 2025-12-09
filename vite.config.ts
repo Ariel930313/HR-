054428d71e1ed363e96b89a1850db2c5b89d86b3
Ariel930313 <b11108016@gapps.ntust.edu.tw>
2025-12-09 11:45:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'client',
+  // 這裡用了 path.resolve，是更強制的寫法，保證抓到 client 資料夾
+  root: path.resolve(__dirname, 'client'),
   build: {
     outDir: '../dist',
     emptyOutDir: true,
