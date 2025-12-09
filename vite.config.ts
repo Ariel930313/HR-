f5d98c493dd6345f8bc0eadb214782c2be844dff
Ariel930313 <b11108016@gapps.ntust.edu.tw>
2025-12-09 12:02:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'client',  // 告訴 Vercel：網頁入口在 client 資料夾
+  // 這裡不需要 root: 'client' 了，因為檔案就在根目錄
   build: {
-    outDir: '../dist', // 告訴 Vercel：打包好的東西放回最外層
-    emptyOutDir: true,
+    outDir: 'dist',
   }
 })
